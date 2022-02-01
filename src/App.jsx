27fb12a6415484cc326067e7af881bfac8f90f00
matias199130 <matiasromero20121991@gmail.com>
2022-02-01 20:04:34 -0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,182 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [currentOperation, setCurrentOperation] = useState("");
+  const [result, setResult] = useState(0);
 
+  function clickNumber(val) {
+    if (currentOperation === "") {
+      setNumber1(number1 + val);
+    } else {
+      setNumber2(number2 + val);
+    }
+  }
+ function allClear(){
+   setNumber1("");
+   setNumber2("");
+   setCurrentOperation("");
+   setResult("");
+ }
+ function borrar(){
+   setResult("");
+   setNumber2("")
+ }
+  function clickOperation(val) {
+    setCurrentOperation(val);
+  }
+
+  function getResult() {
+    switch (currentOperation) {
+      case "+":
+        setResult(Number(number1) + Number(number2));
+        break;
+      case "-":
+        setResult(Number(number1) - Number(number2));
+        break;
+      case "*":
+        setResult(Number(number1) * Number(number2));
+        break;
+      case "/":
+        setResult(Number(number1) / Number(number2));
+        break;
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {" "}
+      {/* contenedor de la calculadora */}
+      <div className="calculator-grid">
+        {" "}
+        {/* contenedor de botones "continene todos los botones"*/}
+        <div className="output">
+          {" "}
+          {/* pantalla de salida  */}
+          <div className="previus-operand">
+            {currentOperation ? number1 + currentOperation : ""}
+          </div>{" "}
+          {/* imput a operar */}
+          <div className="current-operand">
+            {result ? result : !currentOperation ? number1 : number2}
+          </div>{" "}
+          {/* imput ya como resultado */}
+        </div>
+        <button onClick={allClear} className="span-two">
+          AC
+        </button>
+        <button onClick={borrar}>DEL</button>
+        <button
+          onClick={() => {
+            clickOperation("/");
+          }}
+        >
+          /
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(7);
+          }}
+        >
+          7
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(8);
+          }}
+        >
+          8
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(9);
+          }}
+        >
+          9
+        </button>
+        <button
+          onClick={() => {
+            clickOperation("*");
+          }}
+        >
+          *
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(4);
+          }}
+        >
+          4
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(5);
+          }}
+        >
+          5
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(6);
+          }}
+        >
+          6
+        </button>
+        <button
+          onClick={() => {
+            clickOperation("+");
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(1);
+          }}
+        >
+          1
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(2);
+          }}
+        >
+          2
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(3);
+          }}
+        >
+          3
+        </button>
+        <button
+          onClick={() => {
+            clickOperation("-");
+          }}
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(".");
+          }}
+        >
+          .
+        </button>
+        <button
+          onClick={() => {
+            clickNumber();
+          }}
+        >
+          0
+        </button>
+        <button onClick={getResult} className="span-two">
+          =
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
